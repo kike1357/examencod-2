a4afcad606dab511c21aae28cf50095c45c0e2e5
epastorizalorenzo <epastorizalorenzo@atanasoff25.danielcastelao.org>
2015-12-10 10:35:40 +0100

    formateamos el programa

--- src/refactorexamen/RefactorExamen.java
+++ src/refactorexamen/RefactorExamen.java
@@ -15,60 +15,65 @@ public class RefactorExamen {
      * @param args the command line arguments
      */
     public static boolean resultado = false;
+
     public static void main(String[] args) {
         {
-        int digitos=introducirNumDigitos();
-        if(digitos<=0)
-        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
-        for(int i = 1; i <= 99999; i++ )
-        {
-            int auxiliar = i;
- 
-            int contador=numContador(auxiliar);
-        numeroDigitos=contador;
-            
-            
-            if(numeroDigitos==digitos){
-                   if (i < 4) resultado = true;
-            else
-            {
-            if (i % 2 == 0) resultado = false; 
-                else
-                {
-                    int contador1 = 0;  
-                    int i1 = 1; 
-                    int k = (i - 1) / 2; 
-                    if  (k % 2 == 0) k--;  
-             
-                    while(i1 <= k)
-                    {
-                        if (i % i1 == 0) contador1++;
-                            i1 += 2;
-                            if (contador1 == 2) i1 = k + 1;
+            int digitos = introducirNumDigitos();
+            if (digitos <= 0) {
+                System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
+            }
+            for (int i = 1; i <= 99999; i++) {
+                int auxiliar = i;
+
+                int contador = numContador(auxiliar);
+                numeroDigitos = contador;
+
+                if (numeroDigitos == digitos) {
+                    if (i < 4) {
+                        resultado = true;
+                    } else {
+                        if (i % 2 == 0) {
+                            resultado = false;
+                        } else {
+                            int contador1 = 0;
+                            int i1 = 1;
+                            int k = (i - 1) / 2;
+                            if (k % 2 == 0) {
+                                k--;
+                            }
+
+                            while (i1 <= k) {
+                                if (i % i1 == 0) {
+                                    contador1++;
+                                }
+                                i1 += 2;
+                                if (contador1 == 2) {
+                                    i1 = k + 1;
+                                }
+                            }
+
+                            if (contador1 == 1) {
+                                resultado = true;
+                            }
+                        }
                     }
- 
-        if (contador1 == 1) resultado = true;
-            } 
-        } 
-     
-                    resultado(i);    
+
+                    resultado(i);
+                }
             }
         }
-        }
- 
-       
- 
-}
+
+    }
 
     public static void resultado(int i) {
-        if (resultado == true)
+        if (resultado == true) {
             System.out.println(i);
+        }
     }
 
     public static int numContador(int auxiliar) {
-        int contador=0;
-        while (auxiliar != 0)
-        {
+        int contador = 0;
+        while (auxiliar != 0) {
             auxiliar = auxiliar / 10;
             contador++;
         }
@@ -76,10 +81,8 @@ public class RefactorExamen {
     }
 
     public static int introducirNumDigitos() {
-        int digitos=3;
-        int numeroDigitos=0;
+        int digitos = 3;
+        int numeroDigitos = 0;
         return digitos;
     }
-    }
-    
-
+}
